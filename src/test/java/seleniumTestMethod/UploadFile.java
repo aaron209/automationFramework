c3a46a1722662bc163f87469e624e0678d500311
aaron209 <aronsubedi1@gmail.com>
2021-07-22 15:39:06 -0400

    upload file added

---
+++ src/test/java/seleniumTestMethod/UploadFile.java
@@ -0,0 +1,48 @@
+package seleniumTestMethod;
+
+import java.io.File;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import com.google.common.io.Files;
+
+public class UploadFile {
+
+	public WebDriver driver;
+	public String pathAfterUpdate;
+
+	public void uploadFileWithRename(String path) {
+		try {
+			pathAfterUpdate = renameFile(path);
+			File file = new File(pathAfterUpdate);
+			WebElement upload = driver.findElement(By.xpath(""));
+			upload.sendKeys(file.getAbsolutePath());
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			revertFileName(pathAfterUpdate);
+
+		}
+	}
+
+	public String renameFile(String path) throws Exception {
+		DateFormat dateFormat = new SimpleDateFormat("MMddyyHHmmss");
+		Date date = new Date();
+		String currentDate = dateFormat.format(date);
+		String destFile = path.concat(currentDate);
+		File oldFile = new File(path);
+		File newFile = new File(destFile);
+		Files.copy(oldFile, newFile);
+		return destFile;
+
+	}
+
+	public void revertFileName(String path) {
+		File oldFile = new File(path);
+		oldFile.delete();
+	}
+}
