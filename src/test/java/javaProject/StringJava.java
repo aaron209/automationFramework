b51394de4afb75e5927f59dc01e069278fa7d26f
aaron209 <aronsubedi1@gmail.com>
2022-02-03 08:55:58 -0500

    file updated

--- src/test/java/javaProject/StringJava.java
+++ src/test/java/javaProject/StringJava.java
@@ -1,8 +1,31 @@
 package javaProject;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class StringJava {
 
+	// longest sub string with no repetition
+	public static String longestSubString(String str) {
+		Set<Character> set = new HashSet<Character>();
+		String longestTillNow = "";
+		String longestOverall = "";
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			if (set.contains(ch)) {
+				longestTillNow = "";
+				set.clear();
+			}
+			set.add(ch);
+			longestTillNow += ch;
+			if (longestTillNow.length() > longestOverall.length()) {
+				longestOverall = longestTillNow;
+			}
+		}
+		return longestOverall;
+	}
+
 	public static void main(String[] args) {
-		
+		System.out.println(longestSubString("kishorsubedi"));
 	}
 }
