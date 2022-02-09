fb4db3a3558b33e3aa49704921dc911df3cb33c4
aaron209 <aronsubedi1@gmail.com>
2022-02-09 10:42:12 -0500

    file updated

--- src/test/java/javaProject/Reverse.java
+++ src/test/java/javaProject/Reverse.java
@@ -2,26 +2,39 @@ package javaProject;
 
 public class Reverse {
 
-	public static void main(String[]args) {
+	public static void main(String[] args) {
 		String name = "kishor";
 		StringBuffer str = new StringBuffer(name);
 		System.out.println(str.reverse());
-		
-		//other way
-		for(int i =name.length()-1; i>=0; i--) {
+
+		// other way
+		for (int i = name.length() - 1; i >= 0; i--) {
 			System.out.println(name.charAt(i));
 		}
-		
+
 		int num = 462;
 		String strNum = String.valueOf(num);
 		StringBuffer str1 = new StringBuffer(strNum);
 		System.out.println(str1.reverse());
-		
-		//reverse elements of array
-		int [] nums = {23, 54, 67, 45};
-		for(int i=nums.length-1; i>=0; i--) {
+
+		// reverse elements of array
+		int[] nums = { 23, 54, 67, 45 };
+		for (int i = nums.length - 1; i >= 0; i--) {
 			System.out.println(nums[i]);
 		}
 		
+		// reverse only letters
+		String nameReverse = "abcd563efgh673xyz890";
+		String[] namesReverse = nameReverse.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
+		StringBuilder builder = new StringBuilder();
+		for (String names : namesReverse) {
+			try {
+				Integer.parseInt(names);
+				builder.append(names);
+			} catch (Exception e) {
+				builder.append(new StringBuilder(names).reverse());
+			}
+		}
+		System.out.println(builder);
 	}
 }
